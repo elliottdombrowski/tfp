9c7c97792bd793fe583240672269207d21aad237
elliott dombrowski <elliottroyal@me.com>
2022-05-13 00:29:07 -0500

    file structure for header

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>testing</h1>
+      <Router>
+        <Routes>
+          
+        </Routes>
+      </Router>
     </div>
   );
 }
