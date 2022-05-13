67bf1442ec1c0026e33532cb1fb62d91250df5da
elliott dombrowski <elliottroyal@me.com>
2022-05-13 00:30:29 -0500

    test render for header

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+//IMPORTING COMPONENTS
+import Header from './components/Header/Header';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
+        
         <Routes>
-          
+
         </Routes>
       </Router>
     </div>
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.scss';
+import './query.scss';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>testing header</h1>
+    </header>
+  );
+};
+ 
+export default Header;
