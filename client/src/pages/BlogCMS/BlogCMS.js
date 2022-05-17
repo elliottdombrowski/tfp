27f4f30c8e04d40885f2af3d0b73062f8355f4df
elliott dombrowski <elliottroyal@me.com>
2022-05-16 23:58:36 -0500

    set up first input for blog cms

--- client/src/pages/BlogCMS/BlogCMS.js
+++ client/src/pages/BlogCMS/BlogCMS.js
@@ -6,12 +6,15 @@ import './query.scss';
 const BlogCMS = () => {
   return (
     <section className='blog-cms-wrapper'>
-      <div className='blog-cms-header'></div>
+      <div className='blog-cms-header'>
+        <input
+          type='text'
+          className='blog-cms-header'
+        />
+      </div>
       <div className='blog-cms-subheader'></div>
       <div className='blog-cms-body'>
-        <textarea>
-          
-        </textarea>
+        <textarea />
       </div>
     </section>
   );
