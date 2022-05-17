3e985c527003d82812baca949c4b91c8d9d874fb
elliott dombrowski <elliottroyal@me.com>
2022-05-16 23:34:59 -0500

    added blog page / blog cms to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Header from './components/Header/Header';
 
 //IMPORTING PAGES
 import Homepage from './pages/Homepage/Homepage';
+import BlogPage from './pages/BlogPage/BlogPage';
+import BlogCMS from './pages/BlogCMS/BlogCMS';
 
 import './App.css';
 
@@ -17,6 +19,9 @@ function App() {
 
         <Routes>
           <Route exact path='/' element={<Homepage />} />
+
+          <Route exact path='/blog' element={<BlogPage />} />
+          <Route exact path='/blogcms' element={<BlogCMS />} />
         </Routes>
       </Router>
     </div>
