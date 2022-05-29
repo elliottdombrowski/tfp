52328d7e9f4bb03ebadc9d825170ce228d67288e
elliott dombrowski <elliottroyal@me.com>
2022-05-29 15:54:16 -0500

    starting dummy render for tour dates

--- client/src/pages/Shows/Shows.js
+++ client/src/pages/Shows/Shows.js
@@ -3,10 +3,27 @@ import React from 'react';
 import './styles.scss';
 import './query.scss';
 
+const show = [
+  {
+    id: 1,
+    venue: 'house of blues',
+  },
+];
+
 const Shows = () => {
   return (
     <section className='shows-page'>
-      shows
+      <div className='shows-wrapper'>
+        {
+          show.map((shows) => {
+            return (
+              <div className='shows-output' key={shows.id}>
+                <h1>{shows.venue}</h1>
+              </div>
+            );
+          })
+        }
+      </div>
     </section>
   );
 };
