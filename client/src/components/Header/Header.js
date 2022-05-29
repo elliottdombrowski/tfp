d291a8075b0bdd0347c69800aa59d74555077ea7
elliott dombrowski <elliottroyal@me.com>
2022-05-29 15:42:15 -0500

    file structure for shows

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -39,7 +39,7 @@ const Header = () => {
 
         <ul className='main-nav' id='main-nav'>
           <li>
-            <Link to='#'>
+            <Link to='/tour'>
               shows.
             </Link>
           </li>
