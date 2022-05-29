d5ce121c9625eeee908fe93d54b195a9f0630589
elliott dombrowski <elliottroyal@me.com>
2022-05-29 15:30:06 -0500

    navbar scroll state

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,14 +4,35 @@ import { Link } from 'react-router-dom';
 import './styles.scss';
 import './query.scss';
 
+
 const Header = () => {
+
+  window.onscroll = function() {scaleNav()};
+  function scaleNav() {
+    let mainNav = document.getElementById('main-nav');
+    let logo = document.getElementById('logo');
+
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+      // mainNav.style.fontSize = "16px";
+      logo.style.height = "40px";
+      logo.style.width = "250px";
+    } else {
+      // mainNav.style.fontSize = "24px";
+      logo.style.height = "80px";
+      logo.style.width = "500px";
+    }
+  };
+
   return (
     <header>
       <Link to='/'>
-        <div className='logo' />
+        <div 
+          className='logo' 
+          id='logo'
+        ></div>
       </Link>
 
-        <ul className='main-nav'>
+        <ul className='main-nav' id='main-nav'>
           <li>
             <Link to='#'>
               shows.
