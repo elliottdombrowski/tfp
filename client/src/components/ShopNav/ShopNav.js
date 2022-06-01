a98d133525730e607ca4f90edd022c321526e8f3
elliott dombrowski <elliottroyal@me.com>
2022-06-01 15:43:53 -0500

    mapping categories

--- client/src/components/ShopNav/ShopNav.js
+++ client/src/components/ShopNav/ShopNav.js
@@ -3,10 +3,46 @@ import React from 'react';
 import './styles.scss';
 import './query.scss';
 
+const categories = [
+  {
+    id: 1,
+    name: 'Vinyl'
+  },
+  {
+    id: 2,
+    name: 'Cassettes'
+  },
+  {
+    id: 3,
+    name: 'CDs'
+  },
+  {
+    id: 4,
+    name: 'T-Shirts'
+  },
+  {
+    id: 5,
+    name: 'Hoodies'
+  },
+  {
+    id: 6,
+    name: 'Hats'
+  },
+]
+
 const ShopNav = () => {
   return (
-    <nav className='shop-nav'>
-    </nav>
+    <div className='shop-nav'>
+      <ul className='shop-category-list' key={categories.id}>
+        {
+          categories.map((category) => {
+            return (
+              <li>{category.name}</li>
+            );
+          })
+        }
+      </ul>
+    </div>
   );
 };
  
