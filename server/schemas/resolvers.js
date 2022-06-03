8ceccaafb84c2ca8af798e7501f757e8afb1e78c
elliott dombrowski <elliottroyal@me.com>
2022-06-02 19:33:14 -0500

    .

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,16 @@
+const {} = require('../models');
+const axios = require('axios');
+const {} = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
+const { v4: uuidv4 } = require('uuid');
+
+const resolvers = {
+  Query: {
+
+  },
+  Mutation: {
+
+  },
+};
+
+module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -5,12 +5,23 @@ const db = require('./config/connection');
 require('dotenv').config();
 
 const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+server.applyMiddleware({ app });
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
@@ -20,4 +31,9 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
 });
 
-app.listen(PORT, () => { console.log(`Listening on port ${PORT}`) });
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`server running on ${PORT}`);
+    console.log(`GQL playground running at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
