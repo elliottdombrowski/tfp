445f4561b4ef3c94740611e26c60c2275d5e0b2c
elliott dombrowski <elliottroyal@me.com>
2022-06-03 16:54:14 -0500

    fixed crashing GQL

---
+++ server/models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+  },
+  password: {
+    type: String,
+    required: 'password is required',
+    validate: [({ length }) => length >= 5, "Password should be longer."]
+  }
+});
+
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds)
+  }
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,25 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql`
+const typeDefs 
+= gql`
+type User {
+  _id: ID
+  name: String!
+  email: String!
+}
 
+type Auth {
+  token: ID!
+  user: User
+}
+
+type Query {
+  me: User
+}
+
+type Mutation {
+  addUser(name: String!, email: String!, password: String!): Auth
+}
 `;
 
 module.exports = typeDefs;
