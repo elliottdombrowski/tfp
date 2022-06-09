0b18e4044b9589eed23b8ea490e9d5cbbe21f4c8
elliott dombrowski <elliottroyal@me.com>
2022-06-09 18:24:57 -0500

    blog post schema

---
+++ server/models/BlogPost.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const { Schema } = mongoose;
+
+const blogPostSchema = new Schema({
+  poster: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  header: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  subheader: {
+    type: String,
+    trim: true
+  },
+  body: {
+    type: String,
+    trim: true
+  }
+});
+
+const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+
+module.exports = BlogPost;
