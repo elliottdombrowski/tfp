f34eaeea604fe36e3777896aee186e322ce63592
elliott dombrowski <elliottroyal@me.com>
2022-06-09 18:21:02 -0500

    typo in mongo connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/swept', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/tfp', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
