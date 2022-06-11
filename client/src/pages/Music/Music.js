151e7870475dfee5746aed6f59b0cf1de6d8f453
elliott dombrowski <elliottroyal@me.com>
2022-06-10 19:26:36 -0500

    default render for music page

---
+++ client/src/pages/Music/Music.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.scss';
+import './query.scss';
+
+const Music = () => {
+  return (
+    <section className='music-page'>
+      <h1>def render</h1>
+    </section>
+  );
+};
+ 
+export default Music;
